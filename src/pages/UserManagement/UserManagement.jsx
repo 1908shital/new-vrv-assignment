63dce3f06b3e308d2e633ea58b1b8972b816c05a
divspark <divyansh2004mhj@gmail.com>
2024-11-27 11:25:10 +0530

    New 57

--- src/pages/UserManagement/UserManagement.jsx
+++ src/pages/UserManagement/UserManagement.jsx
@@ -4,20 +4,20 @@ import { usersData } from '../../data/data'; // Import data from data.js
 
 const UserManagement = () => {
   return (
-    <div className="user-management-container">
-      <h2 className="page-title">User Management</h2>
+    <div className="user-management-container grid-common">
+      <h2 className="page-title text-white">User Management</h2>
       <div className="cards-container">
         {usersData.map((user, index) => (
-          <div className="user-card" key={index}>
-            <div className="user-card-header">
-              <h3>{user.name}</h3>
-              <span className="user-role">{user.role}</span>
+          <div className="user-card grid-item" key={index}>
+            <div className="user-card-header grid-c-title">
+              <h3 className="user-name text-silver-v1">{user.name}</h3>
+              <span className="user-role text-scarlet">{user.role}</span>
             </div>
             <div className="user-card-body">
-              <p><strong>Email:</strong> {user.email}</p>
-              <p><strong>Date of Birth:</strong> {user.dob}</p>
-              <p><strong>Phone Number:</strong> {user.phone}</p>
-              <p><strong>Permissions:</strong> {user.permissions}</p>
+              <p><strong className="text-silver-v1">Email:</strong> {user.email}</p>
+              <p><strong className="text-silver-v1">Date of Birth:</strong> {user.dob}</p>
+              <p><strong className="text-silver-v1">Phone Number:</strong> {user.phone}</p>
+              <p><strong className="text-silver-v1">Permissions:</strong> {user.permissions}</p>
             </div>
           </div>
         ))}
