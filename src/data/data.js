0d9de3c0a0d6c21aca1aab185a29fcc3cd07ba2b
divspark <divyansh2004mhj@gmail.com>
2024-11-27 11:47:33 +0530

    New 36

--- src/data/data.js
+++ src/data/data.js
@@ -73,31 +73,31 @@ export const budget = [
         id: 19, 
         title: "Subscriptions",
         type: "Automated",
-        amount: 2200000 // Amount in INR
+        amount: 22000 // Amount in INR
     },
     {
         id: 20, 
         title: "Loan Payment",
         type: "Automated",
-        amount: 1600000 // Amount in INR
+        amount: 16000 // Amount in INR
     },
     {
         id: 21, 
         title: "Foodstuff",
         type: "Automated",
-        amount: 2000000 // Amount in INR
+        amount: 20000 // Amount in INR
     },
     {
         id: 22, 
         title: "Subscriptions",
         type: null,
-        amount: 1000000 // Amount in INR
+        amount: 10000 // Amount in INR
     },
     {
         id: 23, 
         title: "Subscriptions",
         type: null,
-        amount: 4000000 // Amount in INR
+        amount: 40000 // Amount in INR
     }
 ];
 
@@ -106,7 +106,7 @@ export const subscriptions = [
         id: 24,
         title: "LinkedIn",
         due_date: "23/12/04",
-        amount: 200000 // Amount in INR
+        amount: 2000 // Amount in INR
     },
     {
         id: 25,
@@ -126,10 +126,10 @@ export const savings = [
     {
         id: 27,
         image: personsImgs.person_one,
-        saving_amount: 25000000, // Amount in INR
+        saving_amount: 25000, // Amount in INR
         title: "Pay Younger Brother’s College Fees",
         date_taken: "23/12/22",
-        amount_left: 4000000 // Amount in INR
+        amount_left: 40000 // Amount in INR
     }
 ]
 
